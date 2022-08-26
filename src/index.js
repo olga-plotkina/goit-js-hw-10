d4ce53663bef6572ca889c637aa05dc79e0d7f28
olga-plotkina <olga.semenyuta@gmail.com>
2022-08-26 23:29:36 +0100

    add fatch

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 import './css/styles.css';
-
 const DEBOUNCE_DELAY = 300;
+const listRef = document.querySelector('.country-list');
+
+const createCountryMarkup = contries => {
+  return contries
+    .map(country => {
+      return `<li><img src="${country.flags.svg}" alt="${country.name.common}" height="20px">${country.name.common}</li>`;
+    })
+    .join('');
+};
+const r = fetch('https://restcountries.com/v3.1/name/sw')
+  .then(response => response.json())
+  .then(countries => {
+    const markup = createCountryMarkup(countries);
+    listRef.innerHTML = markup;
+  })
+  .catch(error => console.log(error));
+
+console.log(r);
